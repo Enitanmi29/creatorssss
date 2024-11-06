10aabac75dee394bfeeb108109f57caf89a53768
tshaba29 <abidakuntaiwoenitan@gmail.com>
2024-11-06 23:28:00 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/creators-frontend/',
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Set output directory to 'build' instead of 'dist'
+  },
 })
