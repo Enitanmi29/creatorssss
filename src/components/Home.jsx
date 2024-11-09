28fe41562caf74f727491ec6998d0e54751864bf
tshaba29 <abidakuntaiwoenitan@gmail.com>
2024-11-09 20:39:43 +0100

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -209,9 +209,9 @@ const Home = () => {
         </div>
 
         <div className='flex flex-col justify-between pb-10 md:pb-20 max-w-fit md:mx-40'>
-                <div className='flex flex-col md:flex-row gap-y-4 md:gap-x-4'>
+                <div className='flex flex-col md:flex-row gap-x-4'>
                     {Advantage.map((advantage, index) => (
-                        <div key={index} className='border rounded-xl bg-white w-full md:w-[365px] p-4'>
+                        <div key={index} className='border rounded-xl bg-white w-full md:w-[365px] p-4 mb-4'>
                             <h1 className='flex justify-center mt-12 mb-4 font-Montserrat font-bold text-[16px] md:text-[20px] text-[#009499]'>
                                 {advantage.header}
                             </h1>
